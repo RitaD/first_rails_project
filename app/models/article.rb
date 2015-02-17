fd84d7846b2ebb0c4e40660e02339525abcc1759
Rita D'Aprano <rita.daprano@gmail.com>
2015-02-17 11:21:01 +0100

    Adds paperclip gem and migrations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+  attr_accessible :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
   def best_comments
     comments.first(2)
